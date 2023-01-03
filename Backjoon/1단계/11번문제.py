c6965a0e5902c61be3dab678474f26b63bd4f98c
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-01-03 14:27:37 +0900

    2023/01/02

---
+++ Backjoon/1단계/11번문제.py
@@ -0,0 +1,9 @@
+a = input()
+b = input()
+
+a = int(a)
+b = int(b)
+
+print(a*(b%10))
+print(a*(b/10)%10)
+print(a*b)
---
+++ Backjoon/1단계/9번문제.py
@@ -0,0 +1,16 @@
+a = 1
+b = 1
+c = 2
+d = 2
+e = 2 
+f = 8
+
+a1, b1, c1, d1, e1, f1 =map(int, input().split()) 
+
+
+print(a-a1)
+print(b-b1)
+print(c-c1)
+print(d-d1)
+print(e-e1)
+print(f-f1)
